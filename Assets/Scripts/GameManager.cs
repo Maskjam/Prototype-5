a8f94a1e91b1c42b80eaee3a03b0c83e9a0c1848
Maskjam <113995112+Maskjam@users.noreply.github.com>
2024-02-15 11:58:48 -0500

    a

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,17 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMpro;
 
 public class GameManager : MonoBehaviour
 {
     
     public List<GameObject> targets;
     private float spawnRate = 1;
+    private int score;
+    public TextMeahProUGUI scoreText;
     
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(SpawnTarget());
+        score = 0;
+        scoreText.trxt
     }
 
     IEnumerator SpawnTarget()
