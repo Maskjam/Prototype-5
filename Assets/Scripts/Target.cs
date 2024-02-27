561605350138948f3cb9ff335b783ea85c50f41b
Maskjam <113995112+Maskjam@users.noreply.github.com>
2024-02-27 12:12:56 -0500

    a

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -11,7 +11,7 @@ public class Target : MonoBehaviour
     private float xRange = 4;
     private float ySpawnPos = -6;
     private GameManager gameManager;
-    public int pointValue;
+    public int pointValue = 1;
     public ParticleSystem explosionParticle;
     // Start is called before the first frame update
     void Start()
